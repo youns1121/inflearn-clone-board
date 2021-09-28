94ef85f0b84981d0f29d95257a69f84060bb0579
younsikMin <minys94@daum.net>
2021-09-28 12:49:49 +0900

    수강신청 엔티티

---
+++ src/main/java/com/ysmin/inflearncloneboard/entity/CourseRegistration.java
@@ -0,0 +1,34 @@
+package com.ysmin.inflearncloneboard.entity;
+
+import com.ysmin.inflearncloneboard.enums.EntityEnums;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Getter
+@Table(name = "course_regi")
+@Entity
+public class CourseRegistration {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "course_regi_id")
+    private Long CourseRegiId;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "registration_stus")
+    private EntityEnums.RegistrationStatus registrationStatus;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_seq")
+    private Member member;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "lecture_id")
+    private Lecture lecture;
+}
