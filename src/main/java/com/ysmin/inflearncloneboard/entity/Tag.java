826291c53980f9c61ae494801be8731a3055fd90
younsikMin <minys94@daum.net>
2021-09-29 17:48:15 +0900

    태그 공통엔티티 상속 추가

--- src/main/java/com/ysmin/inflearncloneboard/entity/Tag.java
+++ src/main/java/com/ysmin/inflearncloneboard/entity/Tag.java
@@ -1,5 +1,6 @@
 package com.ysmin.inflearncloneboard.entity;
 
+import com.ysmin.inflearncloneboard.common.BaseEntity;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -12,7 +13,11 @@ import javax.persistence.*;
 @Getter
 @Table(name = "tag")
 @Entity
-public class Tag {
+public class Tag extends BaseEntity {
+    /**
+     * 태그는 강의를 등록할 수 있는 강사(회원)만 등록 할 수 있음
+     * 일반회원은 등록된 태그를 사용할 수 있음
+     */
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -23,16 +28,9 @@ public class Tag {
     private String tagTitle;
 
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "member_seq")
+    @JoinColumn(name = "instructor_seq")
     private Member member;
 
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "lecture_id")
-    private Lecture lecture;
-
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "community_id")
-    private Community community;
 
 
 }
