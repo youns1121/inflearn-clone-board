c966456bef8406c52f520251f9a744a4235223ed
younsikMin <minys94@daum.net>
2021-10-01 17:04:44 +0900

    컨버터 수정

---
+++ src/main/java/com/ysmin/inflearncloneboard/converter/EnumCommonType.java
@@ -0,0 +1,11 @@
+package com.ysmin.inflearncloneboard.converter;
+
+public interface EnumCommonType {
+
+     /**
+      *
+      * @return
+      */
+     String getStatusCode();
+     String getStatusName();
+}
